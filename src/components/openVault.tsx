5e93216a6ab29bf7de5948e9b05706181267cacc
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2023-05-28 01:47:57 +0300

    feat: open button width

--- src/components/openVault.tsx
+++ src/components/openVault.tsx
@@ -112,7 +112,7 @@ interface IJSON {
           />
         </div>
         {incorrect && <p className="text-center text-red-400 mb-4">incorrect password</p>}
-        <button disabled={!enc} onClick={onOpen} type="button" className="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 ">OPEN</button>
+        <button disabled={!enc} onClick={onOpen} type="button" className="w-full text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2 ">OPEN</button>
       </div>
     )
   }   
